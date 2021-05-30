5a68e5b65b7e643c701be8b8934fe89981a4df51
a-liuxiaohai <1172065083@qq.com>
2021-05-30 16:38:30 +0800

    perf: 优化了输入框的请求

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,9 @@
-// 将对象里面的key值为空的删除，封装的函数，切记不要去改变传入进来的对象
+import { useEffect, useState } from "react";
 
+// 将value===0的返回为true
 export const isFalsy = (value) => (value === 0 ? false : !value);
+
+// 将对象里面的key值为空的删除，封装的函数，切记不要去改变传入进来的对象
 export const cleatEmpty = (obj) => {
   const copyObj = { ...obj };
   Object.keys(copyObj).forEach((key) => {
@@ -12,4 +15,36 @@ export const cleatEmpty = (obj) => {
   });
   console.log(copyObj, 1111);
   return copyObj;
+};
+
+// 自定义hooks，当页面中用到useEffect的替换成自定义的hooks
+export const useMount = (callBack) => {
+  useEffect(() => {
+    callBack();
+  }, []); // 如果是空数组，则代表只请求一次
+};
+
+// 函数防抖
+export const debounce = (fn, delay = 2000) => {
+  let timeoutId;
+  return () => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      fn();
+    }, delay);
+  };
+};
+
+// useDebounce(param,2000)
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setdebounceValue] = useState(value); //初始化debounce的值
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => setdebounceValue(value), delay); //新增了定时器ID
+    return () => clearTimeout(timeoutId); // 每次在上一个useEffect处理完在运行
+  }, [value, delay]); //当传进来的参数发生改变时，会触发effect更新
+  return debounceValue;
 };
--- src/view/projectList/index.jsx
+++ src/view/projectList/index.jsx
@@ -2,32 +2,35 @@ import React, { Fragment, useEffect, useState } from "react";
 import SerchComponent from "./serch";
 import TableComponnet from "./table";
 import qs from "qs";
-import { cleatEmpty } from "../../utils";
+import { cleatEmpty, useMount, useDebounce } from "../../utils";
+
 let userUrl = process.env.REACT_APP_USERURL; // 读取了本地环境变量设置的user地址
 
 const App = () => {
   const [param, setparam] = useState({ name: "", personId: "" });
   const [tableList, settableList] = useState([]);
   const [user, setuser] = useState([]); // 初始化下拉选择的用户信息
+  const debounce = useDebounce(param, 2000);
+  console.log(debounce, "debounce");
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${userUrl}/users`).then(async (res) => {
       if (res.ok) {
         //请求成功之后，需要把table的列表数据保存起来，所以又需要定义一个变量
         setuser(await res.json());
       }
     });
-  }, []); // 当param发生变化的时候，去请求接口
+  });
 
   useEffect(() => {
-    fetch(`${userUrl}/projects?${qs.stringify(cleatEmpty(param))}`).then(
+    fetch(`${userUrl}/projects?${qs.stringify(cleatEmpty(debounce))}`).then(
       async (res) => {
         if (res.ok) {
           settableList(await res.json());
         }
       }
     );
-  }, [param]);
+  }, [debounce]); //当param发生变化时，需要去请求接口
 
   return (
     <Fragment>
